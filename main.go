f42ccc8b25c15eaa51c3558128b318d70b7a875f
Halil Bülent Orhon <halilbulentorhon@gmail.com>
2026-04-03 11:17:21 +0300

    feat: wire up main.go with config, service, and TUI

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/halilbulentorhon/pjf/internal/config"
+	"github.com/halilbulentorhon/pjf/internal/scanner"
+	"github.com/halilbulentorhon/pjf/internal/service"
+	"github.com/halilbulentorhon/pjf/internal/tui"
+)
 
 func main() {
-	fmt.Println("pjf")
+	configPath := config.DefaultPath()
+	cachePath := config.DefaultCachePath()
+
+	cfg, isFirstRun, err := config.Load(configPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Config yüklenemedi: %v\n", err)
+		os.Exit(1)
+	}
+
+	sc := &scanner.FileScanner{}
+	cache := &scanner.JSONCacheStore{Path: cachePath}
+	svc := service.New(cfg, sc, cache)
+
+	if err := tui.Run(svc, configPath, isFirstRun); err != nil {
+		fmt.Fprintf(os.Stderr, "Hata: %v\n", err)
+		os.Exit(1)
+	}
 }
