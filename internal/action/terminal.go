dd21601f15b33d29381124db38aed214de1dd281
Halil Bülent Orhon <halilbulentorhon@gmail.com>
2026-04-03 12:50:47 +0300

    feat: add OpenWithCommand to TerminalOpener for IDE support

--- internal/action/terminal.go
+++ internal/action/terminal.go
@@ -2,4 +2,5 @@ package action
 
 type TerminalOpener interface {
 	Open(dir string) error
+	OpenWithCommand(dir string, command string) error
 }
--- internal/action/terminal_darwin.go
+++ internal/action/terminal_darwin.go
@@ -42,3 +42,43 @@ func openITerm(dir string) error {
 	`
 	return exec.Command("osascript", "-e", script, dir).Run()
 }
+
+func (t *DarwinTerminalOpener) OpenWithCommand(dir string, command string) error {
+	if isITermInstalled() {
+		return openITermWithCommand(dir, command)
+	}
+	return openTerminalWithCommand(dir, command)
+}
+
+func openITermWithCommand(dir string, command string) error {
+	script := `
+		on run argv
+			set targetDir to item 1 of argv
+			set targetCmd to item 2 of argv
+			tell application "iTerm"
+				activate
+				tell current window
+					create tab with default profile
+					tell current session
+						write text "cd " & quoted form of targetDir & " && " & targetCmd
+					end tell
+				end tell
+			end tell
+		end run
+	`
+	return exec.Command("osascript", "-e", script, dir, command).Run()
+}
+
+func openTerminalWithCommand(dir string, command string) error {
+	script := `
+		on run argv
+			set targetDir to item 1 of argv
+			set targetCmd to item 2 of argv
+			tell application "Terminal"
+				activate
+				do script "cd " & quoted form of targetDir & " && " & targetCmd
+			end tell
+		end run
+	`
+	return exec.Command("osascript", "-e", script, dir, command).Run()
+}
